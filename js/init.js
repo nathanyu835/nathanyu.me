4ab05a5a6e4a68ed4d48d9c809ded2da076fd5bd
nathanyu835 <nathan.yu835@gmail.com>
2018-03-28 00:58:41 -0400

    add files

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// code from:http://materializecss.com/modals.html#!
+// additional help from: http://stackoverflow.com/questions/41626343/materialized-modal-not-displaying
+// parallax code from: http://materializecss.com/parallax.html
+// this initializes all modals so they can be triggered
+$(document).ready(function() {
+  $(".dropdown-button").dropdown({ hover: false });
+   $('.modal').modal();
+   $('.parallax').parallax();
+});
